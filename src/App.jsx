883b094db2a40b33d72d92f15c786434d42cf39c
Sam <lionuz669@gmail.com>
2025-03-05 17:58:29 +0900

    Added React Router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import CompanyIntro from "./components/CompanyIntro";
