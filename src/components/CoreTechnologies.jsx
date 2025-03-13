f72f728de088d2746ae4cf7270ad1da29639d39d
Sam <lionuz669@gmail.com>
2025-03-13 14:50:19 +0900

    only picture version

--- src/components/CoreTechnologies.jsx
+++ src/components/CoreTechnologies.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "../styles/CoreTechnologies.scss";
+import placeholderImage from "../images/IRL-02.png";
 
 const CoreTechnologies = () => {
   return (
-    <section id="핵심역량기술" className="core-tech">
-      <h2>핵심역량기술</h2>
-      <img src="/images/tech1.jpg" alt="Technology 1" />
-      <img src="/images/tech2.jpg" alt="Technology 2" />
-      <p>We specialize in advanced exosome research and applications.</p>
+    <section id="핵심역량기술" className="core-section">
+      {/* Full-page image placeholder */}
+      <div className="image-placeholder">
+        <img src={placeholderImage} alt="Core Technologies" />
+      </div>
+
+      {/* Video Element Positioned Exactly on the Image */}
+{/*      <div className="video-container">
+        <video controls autoplay loop muted>
+          <source src="/videos/home-video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div> */}
     </section>
   );
 };
