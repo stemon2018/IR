a5e60f158844f6309805d7c1d8c6edcfefd60bd7
Sam <lionuz669@gmail.com>
2025-03-14 12:59:44 +0900

    pictures updated

--- src/components/CompanyIntro.jsx
+++ src/components/CompanyIntro.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../styles/CompanyIntro.scss";
-import placeholderImage from "../images/IRL-01.png";
+
+// Import all images
+import image1 from "../images/회사소개/IRL-01.png";
+import image2 from "../images/회사소개/IRL-02.png";
+import image3 from "../images/회사소개/IRL-03.png";
+import image4 from "../images/회사소개/IRL-04.png";
+import image5 from "../images/회사소개/IRL-05.png";
+import image6 from "../images/회사소개/IRL-06.png";
+import image7 from "../images/회사소개/IRL-07.png";
+import image8 from "../images/회사소개/IRL-08.png";
 
 const CompanyIntro = () => {
+  const images = [image1, image2, image3, image4, image5, image6, image7, image8];
+
   return (
     <section id="회사소개" className="company-intro-section">
-      {/* Full-page image placeholder */}
-      <div className="image-placeholder">
-        <img src={placeholderImage} alt="Company Introduction Preview" />
-      </div>
+      {images.map((img, index) => (
+        <div key={index} className="image-container">
+          <img src={img} alt={`Company Intro ${index + 1}`} />
+        </div>
+      ))}
     </section>
   );
 };
